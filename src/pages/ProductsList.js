6be925c77694efb4f3dc3e97a35def31914bb3f8
Agustin <agualvila@gmail.com>
2022-02-01 21:28:17 -0300

    adding components

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,7 +1,10 @@
+import SearchBar from '../components/SearchBar/SearchBar';
 function ProductsList() {
   return (
     <>
-      <section className="search-bar">Search</section>
+      <section className="search-bar">
+        <SearchBar />
+      </section>
       <section className="products-list">List</section>
       <section className="button-bar">Button</section>
     </>
