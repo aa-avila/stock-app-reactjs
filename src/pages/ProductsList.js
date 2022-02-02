881ece3f1199a2f31effe37aa585243a945ffd45
Agustin <agualvila@gmail.com>
2022-02-02 12:17:42 -0300

    search input & add button

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,12 +1,39 @@
-import SearchBar from '../components/SearchBar/SearchBar';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Add';
+
 function ProductsList() {
   return (
     <>
       <section className="search-bar">
-        <SearchBar />
+        <TextField
+          fullWidth
+          id="standard-bare"
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            )
+          }}
+        />
       </section>
       <section className="products-list">List</section>
-      <section className="button-bar">Button</section>
+      <section className="button-bar">
+        <IconButton
+          aria-label="Agregar producto"
+          size="large"
+          sx={{
+            backgroundColor: '#6200ef',
+            '&:hover': { backgroundColor: '#7c4dff' },
+            color: '#ffffff'
+          }}
+        >
+          <AddIcon fontSize="inherit" />
+        </IconButton>
+      </section>
     </>
   );
 }
