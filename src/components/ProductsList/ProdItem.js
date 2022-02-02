a633fed0ed494f0176df24a0bd06ce71407070a2
Agustin <agualvila@gmail.com>
2022-02-02 17:51:49 -0300

    productsList sizing & overflow

--- src/components/ProductsList/ProdItem.js
+++ src/components/ProductsList/ProdItem.js
@@ -1,15 +1,15 @@
 import ProdImg from './ProdImg';
 import ProdInfo from './ProdInfo';
 import ProdBtns from './ProdBtns';
-import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
 
 function ProdItem(props) {
   return (
-    <Stack direction="row" justifyContent="space-between" alignItems="center">
+    <Card>
       <ProdImg img={props.img} />
       <ProdInfo title={props.title} price={props.price} stock={props.stock} />
       <ProdBtns />
-    </Stack>
+    </Card>
   );
 }
 
